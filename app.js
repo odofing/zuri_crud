62b3abe85c9440e895d9cde15384bf33356bcd0c
Gbadamosi Odofin <odofing@gmail.com>
2021-05-07 12:24:46 +0100

    modified crud project

--- app.js
+++ app.js
@@ -10,13 +10,10 @@ const postRoute = require('./Routes/Posts');
 app.use('/zuri-crud', postRoute);
 
 
- // mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@mongotesting.1lqon.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,  {useNewUrlParser: true, useUnifiedTopology: true}, () => console.log('connected to remote db!!!'))
+  mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@mongotesting.1lqon.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,  {useNewUrlParser: true, useUnifiedTopology: true}, () => console.log('connected to remote db!!!'))
 
- mongoose.connect(`mongodb+srv://odofing:xgMFSXculTQd9rSu@mongotesting.1lqon.mongodb.net/ZURI_CRUD?retryWrites=true&w=majority`,  {useNewUrlParser: true, useUnifiedTopology: true}, () => console.log('connected to remote db!'))
-
-// connect to db SHELL
- // mongoose.connect('mongodb://localhost:27017/Zuri-Crud', {useNewUrlParser: true, useUnifiedTopology: true}, () => console.log('connected to db!!!'));
+ 
 
 
 
-app.listen( 7000, () => console.log(' server connected!'));
+ app.listen(process.env.PORT || 4000, () => console.log(' server connected!'));
