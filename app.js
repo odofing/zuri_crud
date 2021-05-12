01116aa6420cc8eb8d6b0c4923ed09ca87a8741c
Gbadamosi Odofin <odofing@gmail.com>
2021-05-12 15:50:20 +0100

    updated my task

--- app.js
+++ app.js
@@ -13,7 +13,4 @@ app.use('/zuri-crud', postRoute);
   mongoose.connect(process.env.DB_CONNECTION,  {useNewUrlParser: true, useUnifiedTopology: true}, () => console.log('connected to remote db!!!'))
 
  
-
-
-
  app.listen(process.env.PORT || 4000, () => console.log(' server connected!'));
